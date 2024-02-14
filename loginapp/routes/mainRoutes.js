ef305273c3e42e37d90ad5b4005c17a2e249ce59
Ygencoglu <148764343+Ygencoglu@users.noreply.github.com>
2024-02-14 19:03:37 +0300

    Update mainRoutes.js

--- loginapp/routes/mainRoutes.js
+++ loginapp/routes/mainRoutes.js
@@ -1,4 +1,4 @@
-// routes/mainRoutes.js
+
 const express = require('express');
 const bcrypt = require('bcryptjs');
 const router = express.Router();
@@ -6,34 +6,26 @@ const isLoggedIn = require('../middlewares/authMiddleware');
 const User = require('../models/User');
 const { pool } = require('../utils/dbUtils');
 const flash = require('connect-flash'); // Eğer flash mesajları kullanıyorsanız
-// Ana sayfa
 router.get('/', (req, res) => {
     res.render('index', { message: req.flash('error') });
 });
 
-// Kayıt sayfası
 router.get('/register', (req, res) => {
     res.render('register', { message: req.flash('error') });
 });
 
-// Kayıt işlemi
 router.post('/register', async (req, res) => {
   const { username, password, confirmPassword } = req.body;
 
-  // Şifre doğrulaması kontrolü
   if (password !== confirmPassword) {
     req.flash('error', 'Passwords do not match');
     return res.redirect('/register');
   }
 
   try {
-    // Hash the password
     const hash = await bcrypt.hash(password, 10);
-
-    // Create a new user
     const newUser = new User({ username, password: hash });
 
-    // Save the user to the database
     await newUser.save();
 
     res.redirect('/');
@@ -43,7 +35,7 @@ router.post('/register', async (req, res) => {
     res.redirect('/register');
   }
 });
-// Dashboard
+
 router.get('/dashboard', isLoggedIn, async (req, res) => {
   try {
     const userId = req.user._id;
